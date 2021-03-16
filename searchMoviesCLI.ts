eff4cc3d9945ecbae2ab548c5b71f56d85c1eda2
Muhammad Tanvir Bin Ali <ali2912@outlook.com>
2021-03-16 15:03:39 +0000

    working version 1 of CLI search tool

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,10 +1,56 @@
 import { question } from "readline-sync";
 import { Client } from "pg";
 
+
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-const client = new Client({ database: 'omdb' });
-console.log("Welcome to search-movies-cli!");
 
-const prompt = require("readline-sync").question("What is your name?");
+console.log("Welcome to search-movies-cli!");
+const client = new Client({ database: 'omdb' });
+let searchTerm = ""
+
+const text = "SELECT id, name, date, runtime, budget FROM movies \
+WHERE kind = 'movie' \
+AND LOWER(name) LIKE LOWER($1) \
+ORDER BY date DESC \
+LIMIT 10";
+
+
+async function makeQuery() {
+    let searchTerm = question("What is your favourite movie?")
+    const values = [`%${searchTerm}%`]
+
+    async function execute() {
+        if (searchTerm === 'q') {
+            console.log("Exiting Query!")
+        }
+        else {
+            const result = await client.query(text, values)
+            console.table(result.rows)
+        }
+
+    }
+    await execute()
+    return searchTerm
+}
+
+async function CLI() {
+    await client.connect()
+    console.log("Successfully connected")
+
+    while (searchTerm != 'q') {
+        searchTerm = await makeQuery()
+
+    }
+    await client.end()
+}
+CLI()
+
+
+
+//, revenue, vote_average, votes_count --REMEMBER TO ADD THESE COLUMNS TO SELECT QUERY
+
+
+
+
